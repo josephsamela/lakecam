6c1b518d4de14ffbe7ee5eb85044f51bf9d34e3b
Joseph Samela <josephsamela@gmail.com>
2019-03-19 19:02:36 -0400

    add ifttt notification integration

---
+++ ifttt_instagram_notification.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"value1":"Posted to Instagram"}' https://maker.ifttt.com/trigger/lakecam_instagram/with/key/{webhook}
---
+++ ifttt_livestream_notification.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"value1":"started livestream"}' https://maker.ifttt.com/trigger/lakecam_livestream/with/key/{webhook}
